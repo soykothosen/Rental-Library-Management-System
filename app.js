9377126db78a822767a7cdf4078df874c5e80230
Soykot Hosen <soykothosen1603028@gmail.com>
2019-09-16 05:10:37 +0600

    Work well before adding ejs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
  var bodyParser = require('body-parser');
  var mysql = require("mysql");
 
+ app.set('view engine','ejs');
+
  var db = mysql.createConnection({
   host    : 'localhost',
   user     : 'root',
@@ -21,32 +23,45 @@ db.connect(function(err){
 
  app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+ db.query("SELECT * FROM `student`",function(err, results, fields){
+   	if(err){
+   		//throw err;
+   	}
+   	console.log(results.length);
+   });
+
  app.get('/',function(req,res){
- 	res.sendFile(path.join( __dirname + '/signin.html'));
+ 	res.sendFile(path.join( __dirname + '/ui.html'));
+ });
+  app.get('/studenttshow',function(req,res){
+ 	res.(path.join( __dirname + '/studenttshow.html'));
  });
 
 
- app.post('/',function(req,res){
 
- 	var memid = req.body.mem_id;
- 	var password = req.body.pass;
- 	console.log(memid);
- 	db.query("SELECT pass FROM `persons` WHERE mem_no = ?",[memid],function(err, results, fields){
-   	if(err){
-   		throw err;
-   	}
-   	console.log(results[0].pass);
+ /*app.post('/',function(req,res){
+
+ 	var student = req.body.student;
+ 	//var password = req.body.pass;
+ 	console.log(student);
+ 	//db.query("SELECT pass FROM `persons` WHERE mem_no = ?",[memid],function(err, results, fields){
+   	//if(err){
+   		//throw err;
+   	//}
+   	//console.log(results[0].pass);
    	//var = RowDataPacket.pass
    	//console.log(RowDataPacket.pass);
-   	if (results[0].pass == password) {
+   	//if (results[0].pass == password) {
    		res.send('logIn');
-   	}
-   });
+   	//}
+   //});
  	//var password1 = db.query(SELECT pass FROM `persons` WHERE mem_no = 'memid'); 
  	//res.send(password1);
-    /*res.sendFile(path.join( __dirname + "/student.html"));*/
+    /*res.sendFile(path.join( __dirname + "/student.html"));
     
- });
+ });*/
 
  app.listen('8080',function(err){
  	if(err){
